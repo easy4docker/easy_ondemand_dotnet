304f09fed4365afaa674c1eeffee409695741503
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-04-10 06:54:56 -0700

    Update localSetup.sh

--- localSetup.sh
+++ localSetup.sh
@@ -15,7 +15,7 @@ docker image rm -f local_ondemand-image
 cd ${SCR_DIR}/code/dockerSetting
 docker build -f ${SCR_DIR}/code/dockerSetting/dockerFile -t local_ondemand-image .
 
-echo "{\"code_folder\": \"${SCR_DIR}/code/app\", \"data_folder\": \"${SCR_DIR}7/data\"}" > ${SCR_DIR}/data/_env.json
+echo "{\"code_folder\": \"${SCR_DIR}/code/app\", \"data_folder\": \"${SCR_DIR}/data\"}" > ${SCR_DIR}/data/_env.json
 
 docker run -d --rm -v "${SCR_DIR}/data/sitesShareFolder":/var/_sharedFolder \
 -v "${SCR_DIR}/code/app":/var/_localApp -v "${SCR_DIR}/data":/var/_localAppData --name local_ondemand_container  local_ondemand-image 
